97c9969a11f05b08a1e1e4641bb767d4db53033f
SaintHugo <hugor37@gmail.com>
2020-12-17 09:53:03 -0600

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -5,32 +5,46 @@ import { drawChromeBoiAtCoords, toggleCycling, resize } from './canvasHelpers.js
 export default class ChromeBoisDomain extends Component {
   
   handleMouseMove = (event) => {
-    /* TODO: This method should capture the `x` and `y` coordinates of the mouse
-     * from the event and use them to invoke the `drawChromeBoiAtCoords`
-     * function that has been provided and is already imported
-     * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
-     */
+   /* TODO: This method should capture the `x` and `y` coordinates of the mouse
+   * from the event and use them to invoke the `drawChromeBoiAtCoords`
+   * function that has been provided and is already imported
+   * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
+   */
+    // console.log(event.clientX, event.clientY)
+    drawChromeBoiAtCoords(event.clientX, event.clientY)
+    
   }
   
+  handleClick = () => {
   /* TODO: Create an event handler which, when fired, invokes the provided
    * `toggleCycling` function with no arguments. Don't forget the click event
    * listener that should fire it!
    */
-   
+    // console.log("hi")
+    toggleCycling()
+  }
+
+  handleKeyPress = (e) => {
   /* TODO: Add an event listener to the `<canvas>` element to capture when a key
   /* is pressed. When a key is pressed, an event handler should invoke the
   /* provided `resize` function with a single argument of either '+' or '-'
   /* if the key pressed was 'a', then it should call `resize` with '+'
   /* if the key pressed was 's', then it should call `resize` with '-' 
    */
-  
+    e.key === 'a' ? resize('+') :
+    e.key === 's' ? resize('-') :
+    console.log("neutral")
+  }
+
   render() {
     return (
       <canvas 
+        onClick={this.handleClick}
         onMouseMove={this.handleMouseMove}
+        onKeyPress={this.handleKeyPress}
         width='900'
         height='600'
-        tabIndex="0">
+        tabIndex="0"> 
       </canvas>
     )
   }
